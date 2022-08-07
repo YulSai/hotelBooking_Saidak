ac1cc9fe32670240f7f584f86220a612bf59f618
Yulia Saidak <yluya.guo@gmail.com>
2022-08-07 13:46:31 +0300

    Add exceptions classes

---
+++ src/main/java/com/company/hotelBooking/exceptions/ServicesException.java
@@ -0,0 +1,23 @@
+package com.company.hotelBooking.exceptions;
+
+public class ServicesException extends Exception{
+    public ServicesException() {
+    }
+
+    public ServicesException(String message) {
+        super(message);
+    }
+
+    public ServicesException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServicesException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServicesException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
